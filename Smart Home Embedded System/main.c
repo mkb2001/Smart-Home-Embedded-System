783cfd3da24caccbe6a849fa68d9284a0049cb8d
Kevin Mugarura B <kevinmugarura051@gmail.com>
2024-01-02 01:20:01 +0300

    LCD and SPI confuguration
    Two microprocessors are communicating

--- Smart Home Embedded System/main.c
+++ Smart Home Embedded System/main.c
@@ -6,12 +6,18 @@
  */ 
 
 #include <avr/io.h>
+#include "LCD.h"
 
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
+	LCD_vInit();
+/* Printing Welcome screen */
+LCD_vSend_string("Welcome to smart");
+LCD_movecursor(2,1);
+LCD_vSend_string("home system");
+_delay_ms(1000);//Halt the system for the given time in (ms)
+LCD_clearscreen();//remove all previous    while (1) 
     {
     }
 }
